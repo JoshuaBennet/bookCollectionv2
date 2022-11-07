3c5ef8aa732490aeb5548992b3c529a3dd8871b1
Joshua Bennet <josh@thebennets.net>
2022-11-07 12:12:07 +0000

    made the foundation for the BooksModel and added the the queries for get and put in the BooksModel

---
+++ src/Models/BooksModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+use PDO;
+
+class BooksModel
+{
+    protected PDO $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getAllBooks(): array
+    {
+        $query = $this->db->prepare('SELECT `id`, `name`, `author`, `price`, `image`, `deleted` FROM `books` ORDER BY `author` DESC');
+        $query->execute();
+        return $query->fetchAll();
+    }
+
+    public function addBookToDB()
+    {
+        $query = $this->db->prepare('INSERT INTO `name`, `author`, `price`, `image`');
+        $query->execute();
+    }
+}
